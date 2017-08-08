401a23878c7fb57cfb8ddf6f571d51851d2a6119
paulmillen <pwmillen@gmail.com>
2017-08-08 14:16:05 +0100

    zombie test working

---
+++ test/zombie_test.js
@@ -0,0 +1,16 @@
+const Browser = require ('zombie');
+
+Browser.localhost("test", 3000);
+
+describe('User can see home page', function() {
+
+  const browser = new Browser();
+
+  before(function(done) {
+    browser.visit('/', done);
+  });
+
+  it('should see welcome page', function() {
+    browser.assert.text('title','Makers BnB')
+  });
+});
