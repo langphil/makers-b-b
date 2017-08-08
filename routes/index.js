7c2c4a039aa4f7371de409b478bf70e97208ac69
Imogen Hardy <imogen.hardy@gmail.com>
2017-08-08 14:38:17 +0100

    initial sequelize interface tests

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  models.Listing.create({ name: 'Buckingham Palace', text: 'it is a silly place', img: 'image.jpg'});
+  console.log('listing created');
   res.render('index', { title: 'Makers BnB' });
 });
 
