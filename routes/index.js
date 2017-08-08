e4598d937afc12215c39b7531030cc81dd8d1967
Phil Lang <plangphoto@gmail.com>
2017-08-08 15:14:19 +0100

    Added ability to use and clear test database when testing

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  models.Listing.create({ name: 'Buckingham Palace', text: 'it is a silly place', img: 'image.jpg'});
-  console.log('listing created');
   res.render('index', { title: 'Makers BnB' });
 });
 
