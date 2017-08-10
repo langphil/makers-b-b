85fbd7e8f45932dee8b14886d1809d0d164af8e5
EOSullivanBerlin <esully666@gmail.com>
2017-08-10 16:04:36 +0200

    Forms require all feilds listings

---
+++ test/manditory_feilds_test.js
@@ -0,0 +1,29 @@
+var Browser = require ('zombie');
+var models = require('../models');
+var chai = require('chai');
+var expect = chai.expect;
+
+Browser.localhost("test", 3000);
+
+
+describe('not filling out the form feilds', function(){
+  var browser = new Browser();
+  beforeEach(function(done) {
+    browser.visit('/', done);
+    });
+
+    it('requires a title to submit', function() {
+      browser.fill("title", "Studio Flat in London")
+             .pressButton('add');
+        expect(browser.html("body")).to.not.include("Your listing has been submitted");
+        browser.assert.url({ pathname: '/' });
+    });
+
+
+    it('requires a description to submit', function() {
+      browser.fill("description", "A spacious studio flat with easy access to tube station")
+             .pressButton('add');
+     expect(browser.html("body")).to.not.include("Your listing has been submitted");
+     browser.assert.url({ pathname: '/' });
+    });
+});
